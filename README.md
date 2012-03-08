1b82439120479894b6cf76f3167102c3718a9397
niedhui <dianhui.nie@gmail.com>
2012-03-08 23:43:39 +0800

    add FindById

--- README.md
+++ README.md
@@ -13,7 +13,10 @@ go get github.com/niedhui/mgoo
     coll := session.DB("hello_mgo").C("products")
     coll.RemoveAll(nil)
     coll.Insert(bson.M{"name": "cola", "price": 12})
+
+    result :=mgoo.FindById(coll,"4f54d7db188ffed1c65a1883")
   })
+
 ```
 
 # TODO
--- session.go
+++ session.go
@@ -2,6 +2,7 @@ package mgoo
 
 import (
   "launchpad.net/mgo"
+  "launchpad.net/mgo/bson"
 )
 
 func WithMgo(url string, f func(*mgo.Session)) {
@@ -11,4 +12,18 @@ func WithMgo(url string, f func(*mgo.Session)) {
   }
   defer session.Close()
   f(session)
+}
+
+func FindById(coll *mgo.Collection, id interface{}) (result bson.M) {
+  result = make(bson.M)
+  var queryId bson.ObjectId
+  switch id := id.(type) {
+  case string:
+    queryId = bson.ObjectIdHex(id)
+  case bson.ObjectId:
+    queryId = id
+  default:
+  }
+  coll.Find(bson.M{"_id": queryId}).One(result)
+  return
 }
